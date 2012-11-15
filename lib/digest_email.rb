5a047667d32416b2335095dc77d9a293c5565242
Peter Hamilton <peter@inspiredpixel.net>
2012-11-15 21:45:03 +0000

    Renamed the digest_body file to be digest_items

--- lib/digest_email.rb
+++ lib/digest_email.rb
@@ -1,7 +1,7 @@
 require "digest_email/version"
 require "digest_email/digest"
 require "digest_email/digest_header"
-require "digest_email/digest_body"
+require "digest_email/digest_items"
 require "digest_email/digest_item"
 require "digest_email/digest_footer"
 require "digest_email/digest_parser"
